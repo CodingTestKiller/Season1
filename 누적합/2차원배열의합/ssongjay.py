be0fb76f5dcc596a8ded74726ffebfd2c10c027e
ssongjay <jay5326@naver.com>
2023-01-07 21:54:15 +0900

    2차원 배열의 합 / 40m

--- 누적합/2차원배열의합/ssongjay.py
+++ 누적합/2차원배열의합/ssongjay.py
@@ -1,20 +1,18 @@
 import sys
 
 N, M = [int(x) for x in sys.stdin.readline().rstrip().split()]
-
 arr_2D = [[int(x) for x in sys.stdin.readline().rstrip().split()] for i in range(N)]
 
-print(arr_2D)
-
-# K = int(sys.stdin.readline().rstrip())
-
-sum_2D = []
-
+sum_2D = [[0 for _ in range(M + 1)] for _ in range(N + 1)]
 for i in range(1, N + 1):
     for j in range(1, M + 1):
-        sum_2D[i][j]
+        sum_2D[i][j] = arr_2D[i - 1][j - 1] + sum_2D[i - 1][j] + sum_2D[i][j - 1] - sum_2D[i - 1][j - 1]
 
-sum_2D = sum(arr_2D)
-print(sum_2D)
-# for _ in range(K):
-#     ijxy = [int(x) for x in sys.stdin.readline().rstrip().split()]
+K = int(sys.stdin.readline().rstrip())
+for _ in range(K):
+    ijxy = [int(x) for x in sys.stdin.readline().rstrip().split()]
+    i = ijxy[0]
+    j = ijxy[1]
+    x = ijxy[2]
+    y = ijxy[3]
+    print(sum_2D[x][y] - sum_2D[i - 1][y] - sum_2D[x][j - 1] + sum_2D[i - 1][j - 1])
