e3a5209af6c9ab8439419f6c9b9dca8acf4e5b76
7ngenious <zxvs1218@gmail.com>
2023-01-08 05:01:16 +0900

    2차원 배열의 합

---
+++ 누적합/7ngenious.py
@@ -0,0 +1,27 @@
+# 동적계획법없는 버전 메모리 문제발생
+# n, m = map(int,input().split())
+# arr = [list(map(int, input().split())) for _ in range(n)]
+
+# k=int(input())
+
+# for _ in range(k):
+#     x1, x2, y1, y2 = map(int,input().split())
+#     result = 0
+#     for i in range(x1-1,y1):
+#       for j in range(x2-1,y2):
+#         result += arr[i][j]
+#     print(result)
+
+n, m = map(int,input().split())
+arr = [list(map(int, input().split())) for _ in range(n)]
+
+k=int(input())
+dp = [[0] * (m+1) for _ in range(n+1)]
+for i in range(1, n + 1):
+    for j in range(1, m + 1):
+        dp[i][j] = arr[i-1][j-1] + dp[i-1][j] + dp[i][j-1] - dp[i-1][j-1]
+
+for _ in range(k):
+    x1, x2, y1, y2 = map(int,input().split())
+    result = 0
+    print(dp[y1][y2] - dp[x1-1][y2] - dp[y1][x2-1] + dp[x1-1][x2-1])
