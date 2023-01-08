8f656df594f12883e3615e6f2f484be718793c12
ssongjay <jay5326@naver.com>
2023-01-08 17:12:32 +0900

    2차원 배열의 합 / 40m

--- 누적합/2차원배열의합/ssongjay.py
+++ 누적합/2차원배열의합/ssongjay.py
@@ -1,7 +1,7 @@
 import sys
 
 N, M = [int(x) for x in sys.stdin.readline().rstrip().split()]
-arr_2D = [[int(x) for x in sys.stdin.readline().rstrip().split()] for i in range(N)]
+arr_2D = [[int(x) for x in sys.stdin.readline().rstrip().split()] for _ in range(N)]
 
 sum_2D = [[0 for _ in range(M + 1)] for _ in range(N + 1)]
 for i in range(1, N + 1):
@@ -10,9 +10,5 @@ for i in range(1, N + 1):
 
 K = int(sys.stdin.readline().rstrip())
 for _ in range(K):
-    ijxy = [int(x) for x in sys.stdin.readline().rstrip().split()]
-    i = ijxy[0]
-    j = ijxy[1]
-    x = ijxy[2]
-    y = ijxy[3]
+    i, j, x, y = [int(x) for x in sys.stdin.readline().rstrip().split()]
     print(sum_2D[x][y] - sum_2D[i - 1][y] - sum_2D[x][j - 1] + sum_2D[i - 1][j - 1])
