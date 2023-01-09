a9431ffbb6d48a55217d33da8e75c01d63a372d2
7ngenious <zxvs1218@gmail.com>
2023-01-09 23:45:48 +0900

    Edit: 구간 합 구하기 5 / 10m

--- 누적합/구간합구하기5/7ngenious.py
+++ 누적합/구간합구하기5/7ngenious.py
@@ -7,7 +7,7 @@ dp = [[0] * (n+1) for i in range(n+1)]
 
 for i in range(1, n+1):
     for j in range(1, n+1):
-        dp[i][j] =  arr[i-1][j-1]+dp[i-1][j] + dp[i][j-1] - dp[i-1][j-1]
+        dp[i][j] =  arr[i-1][j-1] + dp[i-1][j] + dp[i][j-1] - dp[i-1][j-1]
 
 for _ in range(m):
     x1, y1, x2, y2 = map(int, input().split())
