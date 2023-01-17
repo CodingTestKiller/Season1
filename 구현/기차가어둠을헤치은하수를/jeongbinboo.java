43b20305cb8712a9478e9a13354aa5f7e016b64c
jeongbinboo <jrkasey3461@naver.com>
2023-01-17 12:10:08 +0900

    add: 기차가 어둠을 헤치고 은하수를 3차 제출 376ms

--- 구현/기차가어둠을헤치은하수를/jeongbinboo.java
+++ 구현/기차가어둠을헤치은하수를/jeongbinboo.java
@@ -12,62 +12,63 @@ public class jeongbinboo
 
         int n = Integer.parseInt(nmSt.nextToken());
         int m = Integer.parseInt(nmSt.nextToken());
-        char trainArr[][] = new char[n][20];
-        StringBuilder seatArr[] = new StringBuilder[n];
-        StringBuilder arrived[] = new StringBuilder[n];
+        int trainArr[] = new int[n];
+        int arrived[] = new int[n];
 
         for(int i = 0; i < n; i++){
-            for(int j = 0; j < 20; j++){
-                trainArr[i][j] = 'X';
-            }
+            trainArr[i] = 0;
         }
         for(int j = 0; j < m; j++){
             StringTokenizer instSt = new StringTokenizer(br.readLine());
             int mode = Integer.parseInt(instSt.nextToken());
             int i = Integer.parseInt(instSt.nextToken()) - 1;
-            if(mode == 1){
-                int x = Integer.parseInt(instSt.nextToken()) - 1;
-                trainArr[i][x] = 'O';
-            } else if(mode == 2) {
-                int x = Integer.parseInt(instSt.nextToken()) - 1;
-                trainArr[i][x] = 'X';
-            } else if(mode == 3){
-                for(int k = 19; k >= 1; k--){
-                    trainArr[i][k] = trainArr[i][k - 1];
-                }
-                trainArr[i][0] = 'X';
-            } else if (mode == 4) {
-                for(int k = 0; k <= 18; k++){
-                    trainArr[i][k] = trainArr[i][k + 1];
-                }
-                trainArr[i][19] = 'X';
+            int x = 0;
+            int isOn = 0;
+
+            switch (mode){
+                case 1:
+                    x = Integer.parseInt(instSt.nextToken()) - 1;
+                    isOn = trainArr[i] & (int)Math.pow(2, 19 - x);
+                    if(isOn == 0){
+                        trainArr[i] += Math.pow(2, 19 - x);
+                    }
+                    break;
+                case 2:
+                    x = Integer.parseInt(instSt.nextToken()) - 1;
+                    isOn = trainArr[i] & (int)Math.pow(2, 19 - x);
+                    if(isOn != 0){
+                        trainArr[i] -= Math.pow(2, 19 - x);
+                    }
+                    break;
+                case 3:
+                    trainArr[i] = trainArr[i] >> 1;
+                    break;
+                case 4:
+                    trainArr[i] = trainArr[i] << 1;
+                    if(trainArr[i] > (Math.pow(2, 20) - 1)){
+                        trainArr[i] -= Math.pow(2, 20);
+                    }
+                    break;
             }
         }
-        for(int i = 0; i < n; i++){
-            StringBuilder seat = new StringBuilder();
-            for(int j = 0; j < 20; j++){
-                seat.append(trainArr[i][j]);
-            }
-            seatArr[i] = seat;
-        }
-        arrived[0] = seatArr[0];
+        arrived[0] = trainArr[0];
         for(int i = 1; i < n; i++){
             flag = 0;
             for(int j = 0; j < count; j++){
-                if(seatArr[i].toString().equals(arrived[j].toString())) {
+                if(arrived[j] == trainArr[i]){
                     flag = 1;
                     break;
                 }
             }
             if(flag == 0){
-                arrived[count] = seatArr[i];
+                arrived[count] = trainArr[i];
                 ++count;
             }
         }
+
         bw.write(Integer.toString(count));
         bw.newLine();
         bw.flush();
         bw.close();
     }
 }
-
