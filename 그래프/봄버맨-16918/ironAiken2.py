25fe800b2750004ab5e62da606cf5fc8230d045d
SungChul Hong <tjd000222@naver.com>
2023-01-28 01:40:49 +0900

    fix wrong answer

--- 그래프/봄버맨-16918/ironAiken2.py
+++ 그래프/봄버맨-16918/ironAiken2.py
@@ -4,54 +4,50 @@ input = stdin.readline
 
 r, c, n = [int(x) for x in input().split(' ')]
 
-map = [['.' for _ in range(c)] for _ in range(r)]
-bombfull_map = [['O' for _ in range(c)] for _ in range(r)]
-map2 = copy.deepcopy(bombfull_map)
+List = [[char for char in input().rstrip()] for _ in range(r)]
+bombfull_List = [['O' for _ in range(c)] for _ in range(r)]
+List2 = copy.deepcopy(bombfull_List)
 cnt = 0
 
-for i in range(r):
-    map[i] = [char for char in input()]
-    del map[i][-1]
-
 if n == 0:
-    for i in range(len(map)):
-        print(''.join(s for s in map[i]))
+    for i in range(len(List)):
+        print(''.join(s for s in List[i]))
     exit()
 
 cnt += 1
 
 if cnt == n:
-    for i in range(len(map)):
-        print(''.join(s for s in map[i]))
+    for i in range(len(List)):
+        print(''.join(s for s in List[i]))
     exit()
 
 while True:
     cnt += 1
 
     if cnt == n:
-        for i in range(len(bombfull_map)):
-            print(''.join(s for s in bombfull_map[i]))
+        for i in range(len(bombfull_List)):
+            print(''.join(s for s in bombfull_List[i]))
         break
 
-    for i in range(len(map)):
-        for j in range(len(map[i])):
-            if map[i][j] == 'O':
-                map2[i][j] = '.'
+    for i in range(len(List)):
+        for j in range(len(List[i])):
+            if List[i][j] == 'O':
+                List2[i][j] = '.'
                 if i > 0:
-                    map2[i - 1][j] = '.'
+                    List2[i - 1][j] = '.'
                 if i < r - 1:
-                    map2[i + 1][j] = '.'
+                    List2[i + 1][j] = '.'
                 if j > 0:
-                    map2[i][j - 1] = '.'
+                    List2[i][j - 1] = '.'
                 if j < c - 1:
-                    map2[i][j + 1] = '.'
+                    List2[i][j + 1] = '.'
 
-    map = copy.deepcopy(map2)
-    map2 = copy.deepcopy(bombfull_map)
+    List = copy.deepcopy(List2)
+    List2 = copy.deepcopy(bombfull_List)
 
     cnt += 1
 
     if cnt == n:
-        for i in range(len(map)):
-            print(''.join(s for s in map[i]))
+        for i in range(len(List)):
+            print(''.join(s for s in List[i]))
         break
