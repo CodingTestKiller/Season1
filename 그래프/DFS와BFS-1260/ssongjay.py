db09f6044ee8ba1928236d1a07c46fd7f3c057f7
ssongjay <jay5326@naver.com>
2023-01-31 16:23:38 +0900

    DFS와 BFS / 2h +

--- 그래프/DFS와BFS-1260/ssongjay.py
+++ 그래프/DFS와BFS-1260/ssongjay.py
@@ -4,7 +4,7 @@ from collections import deque
 def dfs(graph, v, visited):
     visited[v] = True
     print(v, end=' ')
-    for i in graph:
+    for i in graph[v]:
         if not visited[i]:
             dfs(graph, i, visited)
 
@@ -16,8 +16,22 @@ def bfs(graph, start, visited):
         print(v, end=' ')
         for i in graph[v]:
             if not visited[i]:
-                queue.append[i]
+                queue.append(i)
                 visited[i] = True
 
-
 N, M, V = [int(x) for x in sys.stdin.readline().rstrip().split()]
+visited = [False] * (N + 1)
+graph = [[] for _ in range(N + 1)]
+
+for _ in range(M):
+    a, b = [int(x) for x in sys.stdin.readline().rstrip().split()]
+    graph[a].append(b)
+    graph[b].append(a)
+
+for i in graph:
+    i.sort()
+
+dfs(graph, V, visited)
+visited = [False] * (N + 1)
+print()
+bfs(graph, V, visited)
