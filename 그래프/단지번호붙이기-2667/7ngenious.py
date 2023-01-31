8ba94acf382e058cfd378ebd214c0416cc81165d
7ngenious <zxvs1218@gmail.com>
2023-02-01 04:25:23 +0900

    단지번호붙이기 / 2h +

--- 그래프/단지번호붙이기-2667/7ngenious.py
+++ 그래프/단지번호붙이기-2667/7ngenious.py
@@ -3,38 +3,22 @@ import sys
 input = sys.stdin.readline
 n = int(input())
 
-elements = [
-    input().rstrip()
-    for _ in range(n)
-]
+elements = [input().rstrip() for _ in range(n)]
 
-# 상하좌우
 dxs = [1, -1, 0, 0]
 dys = [0, 0, -1, 1]
 
-# queue 
 q = deque()
 
+board = [[] for _ in range(n)]
 
-board = [
-    []
-    for _ in range(n)
-]
-
-# make board with int variable 
 for idx,element in enumerate(elements) :
     for elem in element :
         board[idx].append(int(elem))
 
-visited = [
-    [False] * n 
-    for _ in range(n)
-]
+visited = [[False] * n for _ in range(n)]
 
-home = [
-    [0] * n
-    for _ in range(n)
-]
+home = [[0] * n for _ in range(n)]
 
 def in_range(x, y) :
     return x>=0 and x<n and y>=0 and y<n 
@@ -62,17 +46,12 @@ def bfs() :
                 q.append((nx, ny))
                 home[nx][ny] = cnt 
 
-# 집들이 있는 위치만 추출
 home_pos = []
 
 for i in range(n) :
     for j in range(n) :
         if board[i][j] == 1 :
             home_pos.append((i, j))
-
-# 방문되지 않은 집일 때 bfs()실행
-# bfs()를 통해 인접한 집들을 모두 방문처리 
-# bfs가 끝난 후 cnt += 1
 
 cnt = 0
 for x, y in home_pos :
@@ -94,7 +73,6 @@ for k in range(1, cnt+1) :
                 each_cnt += 1
     cnt_lst.append(each_cnt)
 
-# 오름차순
 cnt_lst.sort()
 
 for elem in cnt_lst :
