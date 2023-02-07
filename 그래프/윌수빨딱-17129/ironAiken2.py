95d2666c64f8f4f70fcce4ca21366a9d8f4847ea
tjdcjf0222 <tjd000222@naver.com>
2023-02-07 18:35:28 +0900

    fix memory error

--- 그래프/윌수빨딱-17129/ironAiken2.py
+++ 그래프/윌수빨딱-17129/ironAiken2.py
@@ -1,38 +1,37 @@
+from collections import deque
 from sys import stdin
 input = stdin.readline
-from collections import deque
 
 n, m = [int(x) for x in input().split(' ')]
 
 map1 = [[x for x in input().rstrip()] for _ in range(n)]
 
 
-move = [[0,1],[0,-1],[1,0],[-1,0]]
-visit = [[False for _ in range(m)] for _ in range(n)]
+move = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+visit = [[0 for _ in range(m)] for _ in range(n)]
+
 
 def bfs(x, y):
     q = deque()
-    q.append([x,y])
-    cnt = 1
+    q.append([x, y])
+    visit[x][y] = 1
 
     while q:
-        for _ in range(len(q)):
-            x, y = q.popleft()
-            visit[x][y] = True
-            
-            for nx, ny in move:
-                if 0 <= x + nx < n and 0 <= y + ny < m and not visit[x+nx][y+ny] and map1[x+nx][y+ny] != '1':
-                    if map1[x + nx][y + ny] == '3' or map1[x + nx][y + ny] == '4' or map1[x + nx][y + ny] == '5':
-                        print('TAK')
-                        print(cnt)
-                        exit()
-                    q.append([x + nx, y + ny])
-        cnt += 1
-    
+        x, y = q.popleft()
+
+        for nx, ny in move:
+            if 0 <= x + nx < n and 0 <= y + ny < m and not visit[x+nx][y+ny] and map1[x+nx][y+ny] != '1':
+                visit[x + nx][y + ny] = visit[x][y]+1
+                q.append([x + nx, y + ny])
+                if map1[x + nx][y + ny] == '3' or map1[x + nx][y + ny] == '4' or map1[x + nx][y + ny] == '5':
+                    print('TAK')
+                    print(visit[x + nx][y + ny] - 1)
+                    return
+
     print('NIE')
 
-for x, value in enumerate(map1):
-    for y, value2 in enumerate(value):
-        if value2 == '2':
-            visit[x][y] = True
-            bfs(x, y)
+
+for i in range(n):
+    for j in range(m):
+        if map1[i][j] == '2':
+            bfs(i, j)
