a58772f3755c2246e06c63b32457b7743118ec20
ssongjay <jay5326@naver.com>
2023-02-08 15:20:02 +0900

    트리 / 2h +

--- 그래프/트리-4256/ssongjay.py
+++ 그래프/트리-4256/ssongjay.py
@@ -4,6 +4,7 @@ input_ = sys.stdin.readline
 
 T = int(input_().rstrip())
 
+
 # preorder에서는 root를
 # inorder에서는 트리를 나눈다
 def get_postorder(preorder, inorder, start, end):
@@ -15,6 +16,7 @@ def get_postorder(preorder, inorder, start, end):
     get_postorder(preorder, inorder, start, root_index - 1)
     get_postorder(preorder, inorder, root_index + 1, end)
     print(root, end=" ")
+
 
 for _ in range(T):
     N = int(input_().rstrip())
