b92009082497ef0dbfce1d62027cd4ee0ff170f8
ssongjay <jay5326@naver.com>
2023-02-08 15:19:46 +0900

    윌리암슨수액빨이딱따구리가 정보섬에 올라온 이유 / 1h +

--- 그래프/윌수빨딱-17129/ssongjay.py
+++ 그래프/윌수빨딱-17129/ssongjay.py
@@ -5,6 +5,7 @@ from collections import deque
 dx = [0, 0, -1, 1]
 dy = [1, -1, 0, 0]
 
+
 def bfs(graph, i, j, visited):
     queue = deque()
     queue.append([i, j])
@@ -13,8 +14,13 @@ def bfs(graph, i, j, visited):
         i, j = queue.popleft()
         for index in range(4):
             nx = i + dx[index]
-            ny = j + dy [index]
-            if 0 <= nx < N and 0 <= ny < M and graph[nx][ny] != 1 and not visited[nx][ny]:
+            ny = j + dy[index]
+            if (
+                0 <= nx < N
+                and 0 <= ny < M
+                and graph[nx][ny] != 1
+                and not visited[nx][ny]
+            ):
                 queue.append([nx, ny])
                 visited[nx][ny] = visited[i][j] + 1
                 if graph[nx][ny] in (3, 4, 5):
@@ -22,6 +28,7 @@ def bfs(graph, i, j, visited):
                     print(visited[i][j])
                     return
     print("NIE")
+
 
 input_ = sys.stdin.readline
 N, M = [int(x) for x in input_().rstrip().split()]
