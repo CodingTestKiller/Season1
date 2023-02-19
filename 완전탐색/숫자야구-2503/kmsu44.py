6cb5d18f2de08b7fa4949c7d57e21f817e83e1fe
kmsu44 <kmsu444@naver.com>
2023-02-19 12:02:54 +0900

    숫자자야구/3h+

---
+++ 완전탐색/숫자야구-2503/kmsu44.py
@@ -0,0 +1,20 @@
+# 솔루션 참고
+import itertools
+n = int(input())
+num_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+think_list = list(itertools.permutations(num_list, 3))
+for _ in range(n):
+    answer, strike, ball = input().split()
+    tmp = []
+    for think in think_list:
+        strike_sum = 0
+        ball_sum = 0
+        for i in range(3):
+            if int(answer[i]) == think[i]:
+                strike_sum += 1
+            elif int(answer[i]) in think:
+                ball_sum += 1
+        if strike_sum == int(strike) and ball_sum == int(ball):
+            tmp.append(think)
+    think_list = tmp
+print(len(think_list))
