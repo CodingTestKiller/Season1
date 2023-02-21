2516aad841f77bf50708b8324b12ac9caaa1d994
SungChul Hong <tjd000222@naver.com>
2023-02-21 23:03:47 +0900

    Revert "Revert "디렉토리 정리""
    This reverts commit b5fb0b0fc4ef4523246de5a1172f70c07e75f037.

---
+++ 완전탐색/숫자야구-2503/ironAiken2.py
@@ -0,0 +1,29 @@
+#메리로 똥싸다가 솔루션 봤습니다 너무 어렵네요,.
+import sys
+from itertools import permutations
+
+n = int(input())
+num = list(permutations([1,2,3,4,5,6,7,8,9], 3))
+
+for _ in range(n):
+    test, s, b = map(int, input().split())
+    test = list(str(test))
+    remove_cnt = 0
+
+    for i in range(len(num)):
+        s_cnt = b_cnt = 0
+        i -= remove_cnt
+
+        for j in range(3):
+            test[j] = int(test[j])
+            if test[j] in num[i]:
+                if j == num[i].index(test[j]):
+                    s_cnt += 1
+                else:
+                    b_cnt += 1
+    
+        if s_cnt != s or b_cnt != b:
+            num.remove(num[i])
+            remove_cnt += 1
+
+print(len(num))
