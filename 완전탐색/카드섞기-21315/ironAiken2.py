1bc4af3d4da02982a0e19b5b6d90740f4c877054
SungChul Hong <tjd000222@naver.com>
2023-02-21 23:54:33 +0900

    3h+

--- 완전탐색/카드섞기-21315/ironAiken2.py
+++ 완전탐색/카드섞기-21315/ironAiken2.py
@@ -1,5 +1,6 @@
 from sys import stdin
 input = stdin.readline
+import math
 
 n = int(input())
 result = [int(x) for x in input().split(' ')]
@@ -7,10 +8,6 @@ card = [i for i in range(1, n+1)]
 
 def mix_card(k, card):
     amount = 2**k
-
-    if amount >= n:
-        return
-    
     recent = amount
 
     card = card[len(card)-amount:] + card[:len(card)-amount]
@@ -25,12 +22,28 @@ def mix_card(k, card):
 
     return card        
     
-for i in range(1, 1000):
+M = int(math.log2(n))
 
-    shuffled_card = mix_card(i, card)
-    for j in range(1, 1000):
-        shuffled_card = mix_card(j, shuffled_card)
+for i in range(1, M + 1):
+    shuffled_card = mix_card(i, card[:])
+    for j in range(1, M + 1):
+        shuffled_card2 = mix_card(j, shuffled_card)
 
-        if result == shuffled_card:
+        if result == shuffled_card2:
             print(i, j)
             exit()
+
+# shuffled_card = mix_card(2, card)
+# shuffled_card = mix_card(2, shuffled_card)
+
+# print(shuffled_card)
+
+
+# 2^k 2^(k - i + 1)
+# 1 2 3 4 5 6 7 8
+# k = 2 / 5 6 7 8 1 2 3 4
+# 7 8 5 6 1 2 3 4
+# 8 7 5 6 1 2 3 4
+# 1 2 3 4 8 7 5 6
+# 3 4 1 2 8 7 5 6
+# 4 3 1 2 8 7 5 6
