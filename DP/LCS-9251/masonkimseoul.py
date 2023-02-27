644729b7180ed115de9aba71f48b2a288ceb738f
masonkimseoul <87306418+masonkimseoul@users.noreply.github.com>
2023-02-28 02:25:24 +0900

    LCS / 2h

---
+++ DP/LCS-9251/masonkimseoul.py
@@ -0,0 +1,20 @@
+import sys
+
+str1=list(sys.stdin.readline().rstrip())
+str2=list(sys.stdin.readline().rstrip())
+len1=len(str1)
+len2=len(str2)
+dp=[[0]*(len1+1) for _ in range(len2+1)]
+
+for i in range(1,len1+1):
+    for j in range(1,len2+1):
+        if str1[i-1]==str2[j-1]:
+            dp[i][j]=dp[i-1][j-1]+1
+        else:
+            dp[i][j]=max(dp[i-1][j],dp[i][j-1])
+
+print(dp[len1][len2])
+
+# 솔루션 참고
+# 초기엔 문자열을 직접 만들어서 덱에 저장하면서 풀려 하였으나... 하도 안돼서 참고함
+# 개수만 세면 됐던 생각보다 간단한 문제
