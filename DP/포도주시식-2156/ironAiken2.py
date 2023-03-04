5ad6b4bea356451d16a8201c5e72c169b32b6b6c
tjdcjf0222 <tjd000222@naver.com>
2023-03-04 19:13:41 +0900

    4h+

---
+++ DP/포도주시식-2156/ironAiken2.py
@@ -0,0 +1,33 @@
+# i > 3 일 경우,
+# 점화식 1, input[i] + input[i-1] + dp[i-3]
+# 점화식 2, input[i] + dp[i-2]
+# 점화식 3, dp[i -1]
+# bottom-up dp 구현
+
+from sys import stdin
+input = stdin.readline
+
+n = int(input())
+dp = [0] * n
+
+wine = [int(input()) for _ in range(n)]
+
+if n == 1:
+    print(wine[0])
+    exit()
+elif n == 2:
+    print(wine[1])
+    exit()
+
+dp[0] = wine[0]
+dp[1] = wine[0] + wine[1]
+dp[2] = max(dp[1], wine[0] + wine[2], wine[1] + wine[2])
+
+for i in range(3, n):
+    first = wine[i] + wine[i-1] + dp[i-3]
+    second = wine[i] + dp[i-2]
+    third = dp[i-1]
+
+    dp[i] = max(first, second, third)
+
+print(dp[-1])
