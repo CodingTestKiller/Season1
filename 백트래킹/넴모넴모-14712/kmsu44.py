b738f5ef09f2b7bb32e45c058bbf901110e74b4f
kmsu44 <kmsu444@naver.com>
2023-03-11 13:26:14 +0900

    계란으로계란치기 / 3h+

--- 백트래킹/넴모넴모-14712/kmsu44.py
+++ 백트래킹/넴모넴모-14712/kmsu44.py
@@ -1,45 +1,26 @@
 # 넴모넴모 (Easy)
 
 n, m = map(int, input().split())
-move = []
-for i in range(n):
-    for j in range(n):
-        move.append((i, j))
-sqare_move = [
-    # 좌상
-    [(-1, -1), (-1, 0), (0, -1)],
-    # 우상
-    [(-1, 0), (-1, 1), (0, 1)],
-    # 좌하
-    [(0, -1),  (1, -1), (1, 0)],
-    # 우하
-    [(0, 1), (1, 0), (1, 1)]
-]
+
+sqare_list = [(0, -1), (-1, 0), (-1, -1)]
+
+visit = [[True] * m for _ in range(n)]
 
 
+def DFS():
+    global cnt
+    cnt += 1
+    for i in range(n):
+        for j in range(m):
+            if visit[i][j]:
+                a, b, c = sqare_list
+                if visit[i+a[0]][j+a[1]] == False and visit[i+b[0]][j+b[1]] == False and visit[i+c[0]][j+c[1]] == False:
+                    return
+                visit[i][j] = False
+                DFS()
+
+
+global cnt
 cnt = 0
-
-
-visit = [[True] * (m) for _ in range(n)]
-for i in range(n):
-    for j in range(n):
-        visit[i][j] = False
-        cnt += 1
-        flag = 0
-        for x, y in move:
-            if x == i and y == j:
-                continue
-            if visit[x][y]:
-                for sqare in sqare_move:
-                    a, b, c = sqare
-                    try:
-                        if visit[x+a[0]][y+a[1]] is False and visit[x+b[0]][y+b[1]] is False and visit[x+c[0]][y+c[1]] is False:
-                            flag = 1
-                            break
-                    except IndexError:
-                        continue
-                if flag == 1:
-                    continue
-                cnt += 1
-                print(x, y)
+DFS()
 print(cnt)
