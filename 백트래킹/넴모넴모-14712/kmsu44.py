ee1ee31e8785f72c4b2bf3f6868be504fe302746
kmsu44 <kmsu444@naver.com>
2023-03-11 17:49:25 +0900

    N-Queen / 3h+ 솔루션

--- 백트래킹/넴모넴모-14712/kmsu44.py
+++ 백트래킹/넴모넴모-14712/kmsu44.py
@@ -2,25 +2,39 @@
 
 n, m = map(int, input().split())
 
-sqare_list = [(0, -1), (-1, 0), (-1, -1)]
+sqare_list = [
+    [(0, -1), (-1, 0), (-1, -1)],
+    [(-1, 0), (-1, 1), (0, 1)],
+    [(0, -1), (1, -1), (1, 0)],
+    [(0, 1), (1, 0), (1, 1)]
+]
 
 visit = [[True] * m for _ in range(n)]
 
 
-def DFS():
+def DFS(x, y):
     global cnt
-    cnt += 1
     for i in range(n):
         for j in range(m):
+            if i == x and j == y:
+                continue
             if visit[i][j]:
-                a, b, c = sqare_list
-                if visit[i+a[0]][j+a[1]] == False and visit[i+b[0]][j+b[1]] == False and visit[i+c[0]][j+c[1]] == False:
-                    return
-                visit[i][j] = False
-                DFS()
+                flag = 0
+                for sqare in sqare_list:
+                    a, b, c = sqare
+                    try:
+                        if visit[i+a[0]][j+a[1]] == False and visit[i+b[0]][j+b[1]] == False and visit[i+c[0]][j+c[1]] == False:
+                            flag = 1
+                            return
+                    except IndexError:
+                        continue
+                if flag == 1:
+                    continue
+                else:
+                    visit[i][j] = False
+                    DFS(i, j)
+                    visit[i][j] = True
 
 
 global cnt
-cnt = 0
-DFS()
-print(cnt)
+DFS(0, 0)
