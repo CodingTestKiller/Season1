3407057d3770753f0f2e4f78650c54cd9a00c8a6
kmsu44 <kmsu444@naver.com>
2023-03-14 22:55:23 +0900

    넴모넴모 / 3h+

--- 백트래킹/넴모넴모-14712/kmsu44.py
+++ 백트래킹/넴모넴모-14712/kmsu44.py
@@ -1,40 +1,29 @@
 # 넴모넴모 (Easy)
 
 n, m = map(int, input().split())
-
-sqare_list = [
-    [(0, -1), (-1, 0), (-1, -1)],
-    [(-1, 0), (-1, 1), (0, 1)],
-    [(0, -1), (1, -1), (1, 0)],
-    [(0, 1), (1, 0), (1, 1)]
-]
-
-visit = [[True] * m for _ in range(n)]
+visit = [[True] * (m+1) for _ in range(n+1)]
 
 
 def DFS(x, y):
     global cnt
-    for i in range(n):
-        for j in range(m):
-            if i == x and j == y:
-                continue
-            if visit[i][j]:
-                flag = 0
-                for sqare in sqare_list:
-                    a, b, c = sqare
-                    try:
-                        if visit[i+a[0]][j+a[1]] == False and visit[i+b[0]][j+b[1]] == False and visit[i+c[0]][j+c[1]] == False:
-                            flag = 1
-                            return
-                    except IndexError:
-                        continue
-                if flag == 1:
-                    continue
-                else:
-                    visit[i][j] = False
-                    DFS(i, j)
-                    visit[i][j] = True
+    if (x, y) == (n+1, 1):
+        cnt += 1
+        return
+    if y == m:
+        nx = x + 1
+        ny = 1
+    else:
+        nx = x
+        ny = y + 1
+    DFS(nx, ny)
+
+    if visit[x-1][y] or visit[x-1][y-1] or visit[x][y-1]:
+        visit[x][y] = False
+        DFS(nx, ny)
+        visit[x][y] = True
 
 
 global cnt
-DFS(0, 0)
+cnt = 0
+DFS(1, 1)
+print(cnt)
