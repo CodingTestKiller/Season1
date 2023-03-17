023dc45bd807b04bc6c551873d01a55fa083e9ab
SungChul Hong <51399982+ironAiken2@users.noreply.github.com>
2023-03-18 05:35:11 +0900

    solution / 그리디 풀이

--- 그리디/A->B/ironAiken2.py
+++ 그리디/A->B/ironAiken2.py
@@ -2,24 +2,20 @@ from sys import stdin
 input = stdin.readline
 
 a, b = [int(x) for x in input().split(' ')]
-
-ans = 10000
-
-
-def find_way(a, b, cnt):
-    global ans
-
-    if a > b:
-        return
-    if a == b:
-        ans = min(ans, cnt)
-
-    find_way(a*10+1, b, cnt+1)
-    find_way(2*a, b, cnt+1)
+cnt = 0
 
 
-find_way(a, b, 0)
-if ans == 10000:
-    ans = -2
+r = 1
+while (b != a):
+    r += 1
+    temp = b
+    if b % 10 == 1:
+        b //= 10
+    elif b % 2 == 0:
+        b //= 2
 
-print(ans + 1)
+    if temp == b:
+        print(-1)
+        break
+else:
+    print(r)
