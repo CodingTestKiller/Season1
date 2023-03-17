a86efc8d92ecb82b943f65a6ad1ffcea14c6726e
kmsu44 <kmsu444@naver.com>
2023-03-17 11:10:33 +0900

    거스름돈 / 15m

---
+++ 그리디/거스름돈/kmsu44.py
@@ -0,0 +1,18 @@
+money = int(input())
+if money < 2:
+    print(-1)
+    exit()
+cnt_5, money = divmod(money, 5)
+print(cnt_5, money)
+if cnt_5 > 0 and money % 2 != 0:
+    cnt_5 -= 1
+    money += 5
+cnt_2, money = divmod(money, 2)
+print(cnt_2, money)
+
+res = cnt_5 + cnt_2
+
+if money != 0:
+    print(-1)
+else:
+    print(res)
