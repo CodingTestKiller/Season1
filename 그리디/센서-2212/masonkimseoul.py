06f35967d383816312e63deaaada02650aef8fc2
masonkimseoul <87306418+masonkimseoul@users.noreply.github.com>
2023-03-21 22:00:10 +0900

    센서 / 1h

---
+++ 그리디/센서-2212/masonkimseoul.py
@@ -0,0 +1,11 @@
+import sys
+from itertools import combinations
+
+N = int(sys.stdin.readline())
+K = int(sys.stdin.readline())
+sensors = list(map(int,sys.stdin.readline().split()))
+sensors.sort()
+distances = [sensors[i+1]-sensors[i] for i in range(N-1)]
+distances.sort(reverse=True)
+print(sum(distances[K-1:]))
+#1h
