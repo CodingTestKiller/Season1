072e394b9bf7efbe18364891d35f0d10ee092add
SungChul Hong <51399982+ironAiken2@users.noreply.github.com>
2023-03-24 17:24:12 +0900

    2h+

---
+++ 투포인터/곂치는건싫어-20922/ironAiken2.py
@@ -0,0 +1,31 @@
+from collections import Counter
+from sys import stdin
+input = stdin.readline
+
+n, k = [int(x) for x in input().split()]
+
+a = [int(x) for x in input().split()]
+
+count = {}
+for data in Counter(a):
+    count[data] = 0
+
+
+start = 0
+end = 0
+ans = 0
+
+
+while end < n:
+
+    if count[a[end]] < k:
+        count[a[end]] += 1
+        end += 1
+
+    else:
+        count[a[start]] -= 1
+        start += 1
+    ans = max(end-start, ans)
+
+
+print(ans)
