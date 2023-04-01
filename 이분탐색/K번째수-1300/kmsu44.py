d473c6aa981619e4b5fbfa49a8f75deda45c64be
kmsu44 <kmsu444@naver.com>
2023-04-01 14:53:40 +0900

    chore : code수정

---
+++ 이분탐색/K번째수-1300/kmsu44.py
@@ -0,0 +1,15 @@
+n = int(input())
+k = int(input())
+left = 1
+right = min(10**9, n * n)
+while left <= right:
+    mid = (left + right)//2
+    cnt = 0
+    for i in range(1, n+1):
+        cnt += min(mid // i, n)
+    if cnt >= k:
+        right = mid - 1
+    else:
+        left = mid + 1
+
+print(left)
