74adc11512081049dbc8879aedb4a676ebd72325
7ngenious <zxvs1218@gmail.com>
2023-04-04 22:07:44 +0900

    입국심사/ 1h 30m+

---
+++ 이분탐색/입국심사-3079/7ngenious.py
@@ -0,0 +1,26 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+arr = [int(input()) for _ in range(n)]
+
+def check_out(n,m,arr):
+  left, right = min(arr), max(arr) * m
+  ans = right
+
+  while left <= right:
+      total = 0
+      mid = (left + right) // 2
+
+      for i in range(n):
+          total += mid // arr[i]
+
+      if total >= m:
+          right = mid -1
+          ans = min(mid, ans)
+
+      else:
+          left = mid + 1
+      return ans
+result = check_out(n,m,arr)
+print(result)
