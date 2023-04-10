934b47e3197a77359adb064770b9e33ce1097a94
yireal <andytwo9878@gmail.com>
2023-04-10 23:29:55 +0900

    [이우식] 궁금한민호-1507, 지름길-1446, 특정거리의도시찾기-18352, 플로이드-11404

--- 최단거리/플로이드-11404/yireal.py
+++ 최단거리/플로이드-11404/yireal.py
@@ -4,6 +4,33 @@ inp = sys.stdin.readline
 n = int(inp())
 m = int(inp())
 INF = 1e9
+graph = [[INF]*(n+1) for _ in range(n+1)]
+for i in range(m):
+    s,e,c = map(int,inp().split())
+    if graph[s][e] > c:
+        graph[s][e] = c
+for k in range(1,n+1):
+    for i in range(1,n+1):
+        for j in range(1,n+1):
+            if i!=j and graph[i][j] > graph[i][k] + graph[k][j]:
+                graph[i][j] = graph[i][k] + graph[k][j]
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        if graph[i][j] == INF:
+            print(0,end=' ')
+        else:
+            print(graph[i][j],end=' ')
+    print()
+#플로이드-월셔 사용한 풀이 400ms python
+            
+
+"""
+import sys
+import heapq
+inp = sys.stdin.readline
+n = int(inp())
+m = int(inp())
+INF = 1e9
 graph = [[] for _ in range(n+1)]
 for _ in range(m):
     start,end,pay = map(int,inp().split())
@@ -28,3 +55,5 @@ for i in range(1,n+1):
         else:
             print(cost[i],end = ' ')
     print('')
+"""
+#다익스트라 사용한 풀이 800ms pypy3
